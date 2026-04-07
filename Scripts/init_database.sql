00b136d9e88378714d763fad2fc43556747f1f9e
Avulapati kiran Kumar <101157468+kirankumarchowdary2@users.noreply.github.com>
2026-04-07 12:26:09 +0530

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,36 @@
+/*
+===================================================
+Create Database/Catalog and Schemas
+===================================================
+Script Purpose:
+  This script create the catalog if the same catalog exists with same name it drops and then create the new catalog
+  If it dropped the exsiting catalog and then create the three schema's inside of the database/catalog :Bronze,Silver,Gold
+Warning:
+  If database exists and have the data then the data will be dropped.
+====================================================
+
+
+D
+---Using the newly created Catalog/Database---sql_data_warehouse
+  
+use catalog sql_datawarehouse;
+
+%md
+### Checking whether the catalog exists with same name if exists drop the catalog 
+%sql
+drop catalog if exists sql_datawarehouse cascade;
+%md
+### Create the catalog/Database 
+%sql
+create catalog sql_datawarehouse
+%md
+### Use the created catalog/database for the project
+%sql
+use catalog sql_datawarehouse;
+%md
+### Create the three schema's Layers _Bronze,Silver,Gold_
+%sql
+create schema bronze;
+%sql
+create schema silver;
+create schema gold;
